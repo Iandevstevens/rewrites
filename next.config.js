412f742f72c38dd4453a9939c19c3f46b521c96b
Ian Stevens <iandevstevens@gmail.com>
2022-03-23 07:01:31 +0200

    rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/about2",
+        destination: "https://www.youtube.com/watch?v=R59e1Vl5lO8",
+      },
+    ];
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
