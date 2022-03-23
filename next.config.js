31e6342e8a24c5dcf812203f91eac56dd4f9b9d1
Ian Stevens <iandevstevens@gmail.com>
2022-03-23 07:29:00 +0200

    test api rewrite

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         source: "/about2",
         destination: "https://www.youtube.com/embed/tgbNymZ7vqY",
       },
+      {
+        source: "/s/player/293baa5d/www-player.css",
+        destination: "https://www.youtube.com/s/player/293baa5d/www-player.css",
+      },
     ];
   },
   reactStrictMode: true,
